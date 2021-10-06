f4acdd3e85409d03119dc04e24e6e160eec4d042
natalka1122 <natalka1122@yandex.ru>
2021-10-06 18:44:37 +0300

    simple frontend app

--- frontend/main.py
+++ frontend/main.py
@@ -3,6 +3,23 @@ from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-    return render_template('main.jinja', name="vasya")
+    data = [
+        {
+            "date": "date111",
+            "code": "code",
+            "m_name": "m_name",
+            "bbb": "bbb",
+            "sss": "sss",
+        },
+        {
+            "date": "date222",
+            "code": "code",
+            "m_name": "m_name",
+            "bbb": "bbb",
+            "sss": "sss",
+        },
+    ]
+    return render_template("main.jinja", name="vasya", data=data)
